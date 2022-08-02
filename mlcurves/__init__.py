3d4c37838367efd2b25153f932abd7b57410301e
Jason St George <stgeorge@brsc.com>
2022-08-02 14:50:44 -0400

    update init for utils

--- mlcurves/__init__.py
+++ mlcurves/__init__.py
@@ -1,3 +1,4 @@
+from joblib import parallel_backend
 from .cluster import cluster #tsne, pca, pca_then_tsne, pca_3D, correlation_heatmap, dbscan
 from .complexity_curves import complexity_curves_npy, complexity_curves_tf
 from .train_curves import train_set_size_curves_npy, train_set_size_curves_tf
@@ -5,3 +6,7 @@ from .learnrate_schedule_curves import learn_rate_range_test, AddLearningRateToH
 from .models import antirectifier # import build_antirectifier_cnn_1D, build_antirectifier_cnn_2D
 from .models import convnext
 from .models import vit
+from .curve_utils import env, permutation, random_permutation, sample_pd
+from .curve_utils import create_random_indices, shuffle_sk, shufflej, vec_matrix
+from .curve_utils import take_subset, plot_metrics, mnist_model, mnist_tfds, mnist_npy
+from .curve_utils import fargs, timestamp, product, get_param_count, batch_generator
